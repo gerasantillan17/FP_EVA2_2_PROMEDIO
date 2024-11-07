7691e3309635f21c1c6865f041e0cfbbe7b09718
gerasantillan17 <gerapumas122@gmail.com>
2024-11-06 23:53:09 -0600

    Add files via upload

---
+++ JavaApplication28.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication28;
+
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author gerar
+ */
+public class JavaApplication28 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  int califa = 0, total;
+  Scanner captu = new Scanner(System.in);
+  
+  total=0;
+  for(int i=1; i<=30;  i++){
+      System.out.println("Estudiante # "+1);
+      System.out.println("Calificacion:");
+      captu.nextInt();
+      captu.nextLine();
+      total = total + califa;
+      System.out.println("Suma calificaciones = " + total);
+      
+      double promedio;
+      
+      
+      
+  }
+  }
+    }
+    
